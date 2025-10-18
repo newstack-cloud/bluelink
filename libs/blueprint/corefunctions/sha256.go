2227ed93291fdf697cab03e33de1d0039cd78529
Andre Sutherland <andrelsf95@gmail.com>
2025-10-18 14:19:38 +0100

    feat(blueprint): add sha256 function

---
+++ libs/blueprint/corefunctions/sha256.go
@@ -0,0 +1,96 @@
+package corefunctions
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/newstack-cloud/bluelink/libs/blueprint/function"
+	"github.com/newstack-cloud/bluelink/libs/blueprint/provider"
+)
+
+// SHA256Function provides the implementation of
+// a function that computes the SHA-256 hash of input data.
+type SHA256Function struct {
+	definition *function.Definition
+}
+
+// NewSHA256Function creates a new instance of the SHA256Function with
+// a complete function definition.
+func NewSHA256Function() provider.Function {
+	return &SHA256Function{
+		definition: &function.Definition{
+			Description: "A function that computes the SHA-256 hash of the input data and returns it as a hexadecimal string.",
+			FormattedDescription: "A function that computes the SHA-256 hash of the input data and returns it as a hexadecimal string.\n\n" +
+				"**Examples:**\n\n" +
+				"Computing hash of a string:\n" +
+				"```\n${sha256(\"Hello World\")}\n```\n\n" +
+				"Computing hash of binary data:\n" +
+				"```\n${sha256(base64decode(datasources.certificate.certificateBody))}\n```\n\n" +
+				"Using hash for resource naming:\n" +
+				"```\nmy-bucket-${sha256(variables.environment)}\n```",
+			Parameters: []function.Parameter{
+				&function.AnyParameter{
+					Label: "data",
+					UnionTypes: []function.ValueTypeDefinition{
+						&function.ValueTypeDefinitionScalar{
+							Label: "string",
+							Type:  function.ValueTypeString,
+						},
+						&function.ValueTypeDefinitionScalar{
+							Label: "bytes",
+							Type:  function.ValueTypeBytes,
+						},
+					},
+					Description: "The data to compute the hash for. This can be a string or a byte array.",
+				},
+			},
+			Return: &function.ScalarReturn{
+				Type: &function.ValueTypeDefinitionScalar{
+					Label: "string",
+					Type:  function.ValueTypeString,
+				},
+				Description: "The SHA-256 hash as a hexadecimal string.",
+			},
+		},
+	}
+}
+
+func (f *SHA256Function) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *SHA256Function) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var value any
+	if err := input.Arguments.GetVar(ctx, 0, &value); err != nil {
+		return nil, err
+	}
+
+	var data []byte
+	switch v := value.(type) {
+	case string:
+		data = []byte(v)
+	case []byte:
+		data = v
+	default:
+		return nil, function.NewFuncCallError(
+			fmt.Sprintf("input argument at index 0 must be a string or byte array, got %T", value),
+			function.FuncCallErrorCodeInvalidArgumentType,
+			input.CallContext.CallStackSnapshot(),
+		)
+	}
+
+	hash := sha256.Sum256(data)
+	return &provider.FunctionCallOutput{
+		ResponseData: hex.EncodeToString(hash[:]),
+	}, nil
+}
---
+++ libs/blueprint/corefunctions/sha256_test.go
@@ -0,0 +1,106 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/newstack-cloud/bluelink/libs/blueprint/core"
+	"github.com/newstack-cloud/bluelink/libs/blueprint/function"
+	"github.com/newstack-cloud/bluelink/libs/blueprint/internal"
+	"github.com/newstack-cloud/bluelink/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type SHA256FunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&SHA256FunctionTestSuite{})
+
+func (s *SHA256FunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &core.ParamsImpl{},
+		registry: &internal.FunctionRegistryMock{
+			Functions: map[string]provider.Function{},
+			CallStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *SHA256FunctionTestSuite) Test_computes_sha256_hash_of_string(c *C) {
+	sha256Func := NewSHA256Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "sha256",
+	})
+	output, err := sha256Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args:    []any{"Hello World"},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	// SHA-256 of "Hello World"
+	c.Assert(output.ResponseData, Equals, "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e")
+}
+
+func (s *SHA256FunctionTestSuite) Test_computes_sha256_hash_of_bytes(c *C) {
+	sha256Func := NewSHA256Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "sha256",
+	})
+	output, err := sha256Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				[]byte("test data"),
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	// SHA-256 of "test data"
+	c.Assert(output.ResponseData, Equals, "916f0027a575074ce72a331777c3478d6513f786a591bd892da1a577bf2335f9")
+}
+
+func (s *SHA256FunctionTestSuite) Test_computes_sha256_hash_of_empty_string(c *C) {
+	sha256Func := NewSHA256Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "sha256",
+	})
+	output, err := sha256Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args:    []any{""},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	// SHA-256 of empty string
+	c.Assert(output.ResponseData, Equals, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+}
+
+func (s *SHA256FunctionTestSuite) Test_returns_error_for_invalid_argument_type(c *C) {
+	sha256Func := NewSHA256Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "sha256",
+	})
+	_, err := sha256Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args:    []any{12345},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, NotNil)
+	funcErr, isFuncErr := err.(*function.FuncCallError)
+	c.Assert(isFuncErr, Equals, true)
+	c.Assert(funcErr.Message, Matches, "input argument at index 0 must be a string or byte array.*")
+	c.Assert(funcErr.Code, Equals, function.FuncCallErrorCodeInvalidArgumentType)
+}
