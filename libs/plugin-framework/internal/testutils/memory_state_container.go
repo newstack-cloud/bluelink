1462644119808c7c5bc25e0ad23356de1f7135a3
Andre Sutherland <andrelsf95@gmail.com>
2026-01-02 02:13:48 +0000

    test(plugin-framework): update test in-memory state container to include computed fields

--- libs/plugin-framework/internal/testutils/memory_state_container.go
+++ libs/plugin-framework/internal/testutils/memory_state_container.go
@@ -919,6 +919,9 @@ func copyResource(resourceState *state.ResourceState) state.ResourceState {
 	dependsOnChildren := make([]string, len(resourceState.DependsOnChildren))
 	copy(dependsOnChildren, resourceState.DependsOnChildren)
 
+	computedFields := make([]string, len(resourceState.ComputedFields))
+	copy(computedFields, resourceState.ComputedFields)
+
 	return state.ResourceState{
 		ResourceID:         resourceState.ResourceID,
 		Name:               resourceState.Name,
@@ -929,6 +932,7 @@ func copyResource(resourceState *state.ResourceState) state.ResourceState {
 		PreciseStatus:      resourceState.PreciseStatus,
 		Description:        resourceState.Description,
 		Metadata:           &metadataCopy,
+		ComputedFields:     computedFields,
 		DependsOnResources: dependsOnResources,
 		DependsOnChildren:  dependsOnChildren,
 		FailureReasons:     resourceState.FailureReasons,
